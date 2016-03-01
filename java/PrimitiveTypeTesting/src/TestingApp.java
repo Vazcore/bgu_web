e9ad4938253caefb0e111c579d04d140fb19ff7d
Vazcore <vazcore@live.ru>
2016-03-01 22:07:18 +0300

    Java Lab2

--- java/PrimitiveTypeTesting/src/TestingApp.java
+++ java/PrimitiveTypeTesting/src/TestingApp.java
@@ -38,6 +38,10 @@ class Tester{
         System.out.printf(val1+"!="+val2+" => %b\n", val1!=val2);
         System.out.printf(val1+">"+val2+" => %b\n", val1>val2);
         System.out.printf(val1+"<"+val2+" => %b\n", val1<val2);
+        System.out.println(val1 + " >> " + val2 + " => " + (val1 >> val2));
+        System.out.println(val1 + " << " + val2 + " => " + (val1 << val2));
+        System.out.println(val1 + " & " + val2 + " => " + (val1 & val2));
+        System.out.println(val1 + " | " + val2 + " => " + (val1 | val2));
         System.out.printf(val1+" += "+val2+" => %d\n", temp += val2);
         temp = val1;
         System.out.printf(val1+" -= "+val2+" => %d\n", temp -= val2);
@@ -63,7 +67,7 @@ class Tester{
      * Operations between 2 booleans
      */
     public static void test(boolean val1, boolean val2){
-        System.out.println("-----Operations booleans integers-------------");
+        System.out.println("----- Operations booleans -------------");
         System.out.println("val1 = true, val2 = false \n");
         System.out.printf("%b == %b => %b\n", val1, val2, val1 == val2);
         System.out.printf("%b != %b => %b\n", val1, val2, val1 != val2);
@@ -74,6 +78,40 @@ class Tester{
         System.out.printf("!%b != %b => %b\n", val1, val2, !val1 != val2);
         System.out.println("----------------------------------------");
     }
+    
+    public static void test(char ch1, char ch2)
+    {
+        System.out.println("----- Operations char -------------");
+        System.out.println(ch1 + " = " + (int)ch1 );
+        System.out.println(ch2 + " = " + (int)ch2 );
+        System.out.println(ch1 + " + " + ch2 + " => " + (ch1+ch2));
+        System.out.println(ch1 + " - " + ch2 + " => " + (ch1-ch2));
+        System.out.println(ch1 + " / " + ch2 + " => " + (ch1/ch2));
+        System.out.println(ch1 + " * " + ch2 + " => " + (ch1*ch2));
+        System.out.println(ch1 + " * " + 5.2 + " => " + (ch1*5.2));
+        System.out.println(ch1 + " >> " + ch2 + " => " + (ch1>>ch2));
+        System.out.println(ch1 + " << " + ch2 + " => " + (ch1<<ch2));
+        System.out.println(ch1 + " == " + ch2 + " => " + (ch1==ch2));
+    }
+    
+    public static void test(byte b1, byte b2)
+    {
+        System.out.println("----- Operations byte -------------");        
+        test((int)b1, (int)b2);
+    }
+    
+    public static void test(short b1, short b2)
+    {
+        System.out.println("----- Operations short -------------");        
+        test((int)b1, (int)b2);
+    }
+    
+    public static void test(long b1, long b2)
+    {
+        System.out.println("----- Operations long -------------");        
+        test((int)b1, (int)b2);
+    }
+   
 }
 
 public class TestingApp {
@@ -83,9 +121,20 @@ public class TestingApp {
      */
     public static void main(String[] args) {
         
+        // Integer
         Tester.test(5, 3);
+        // Integer negative
         Tester.test(-8, 3);   
-        Tester.test(true, false);  
+        // Boolean
+        Tester.test(true, false); 
+        // Char
+        Tester.test('f', 'd');
+        // Byte
+        Tester.test((byte)7, (byte)3);        
+        //Short
+        Tester.test((short)22,(short)11);
+        //Long
+        Tester.test((long)25,(long)15);
     }
     
 }
